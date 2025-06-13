21b373613a57b0e09577d4552528671acfe2cdeb
Hj <helkin.jimenez@correounivalle.edu.co>
2025-06-13 18:23:41 -0500

    Movida la función generarCadenas dentro de reconstruirCadenaIngen

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -6,20 +6,18 @@ package object ReconstCadenas {
 // Definimos el alfabeto sobre el que trabaja el genoma
 val alfabeto: Seq[Char] = Seq('a', 'c', 'g', 't')
 
-// Tipo para representar el oráculo
 type Oraculo = Seq[Char] => Boolean
 
-// Función auxiliar que genera todas las combinaciones posibles de longitud `n`
-def generarCadenas(n: Int): Seq[Seq[Char]] = {
-  if (n == 0) Seq(Seq.empty)
-  else for {
-    prefijo <- generarCadenas(n - 1)
-    letra <- alfabeto
-  } yield prefijo :+ letra
-}
-
-// Implementación funcional y pura del algoritmo ingenuo
 def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
+
+  def generarCadenas(k: Int): Seq[Seq[Char]] = {
+    if (k == 0) Seq(Seq.empty)
+    else for {
+      prefijo <- generarCadenas(k - 1)
+      letra <- alfabeto
+    } yield prefijo :+ letra
+  }
+
   generarCadenas(n).find(o).getOrElse(Seq.empty)
 }
 
