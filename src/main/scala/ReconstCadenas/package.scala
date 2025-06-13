c4342ab222d8c671ce3943449f1f9e16fcc13851
Hj <helkin.jimenez@correounivalle.edu.co>
2025-06-13 17:37:03 -0500

    Cambios en submódulo HGJ

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -3,11 +3,25 @@ import Oraculo._
 
 package object ReconstCadenas {
 /*
-  def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
-    // Recibe la longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
-    // y devuelve la secuencia reconstruida
-    ???
-  }
+Definimos el alfabeto sobre el que trabaja el genoma
+val alfabeto: Seq[Char] = Seq('a', 'c', 'g', 't')
+
+// Tipo para representar el oráculo
+type Oraculo = Seq[Char] => Boolean
+
+// Función auxiliar que genera todas las combinaciones posibles de longitud `n`
+def generarCadenas(n: Int): Seq[Seq[Char]] = {
+  if (n == 0) Seq(Seq.empty)
+  else for {
+    prefijo <- generarCadenas(n - 1)
+    letra <- alfabeto
+  } yield prefijo :+ letra
+}
+
+// Implementación funcional y pura del algoritmo ingenuo
+def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
+  generarCadenas(n).find(o).getOrElse(Seq.empty)
+}
 
   def reconstruirCadenaMejorado(n: Int, o: Oraculo): Seq[Char] = {
     // Recibe la longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
