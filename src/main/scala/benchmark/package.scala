614dcbb3fee926a1c63abf6d20c1afe06798e2e5
Daniel Andrade <andrade.daniel@correounivalle.edu.co>
2025-06-15 18:47:41 -0500

    Benchmark agregado

--- src/main/scala/benchmark/package.scala
+++ src/main/scala/benchmark/package.scala
@@ -6,19 +6,19 @@ import org.scalameter._
 
 package object benchmark {
 
-  type AlgoritmoIngenuo = (Int, Oraculo) => Seq[Char]
+  type Algoritmo = (Int, Oraculo) => Seq[Char]
   
-  def compararAlgoritmos(a1: AlgoritmoIngenuo, a2: AlgoritmoIngenuo)
+  def compararAlgoritmos(a1: Algoritmo, a2: Algoritmo)
                         (tamano: Int, oraculo: Oraculo): (Double, Double, Double) = {
     val timeA1 = config(
-      KeyValue(Key.exec.minWarmupRuns -> 1),
-      KeyValue(Key.exec.maxWarmupRuns -> 1),
+      KeyValue(Key.exec.minWarmupRuns -> 10),
+      KeyValue(Key.exec.maxWarmupRuns -> 20),
       KeyValue(Key.verbose -> false)
     ) withWarmer (new Warmer.Default) measure (a1(tamano, oraculo))
 
     val timeA2 = config(
-      KeyValue(Key.exec.minWarmupRuns -> 1),
-      KeyValue(Key.exec.maxWarmupRuns -> 1),
+      KeyValue(Key.exec.minWarmupRuns -> 10),
+      KeyValue(Key.exec.maxWarmupRuns -> 20),
       KeyValue(Key.verbose -> false)
     ) withWarmer (new Warmer.Default) measure (a2(tamano, oraculo))
 
