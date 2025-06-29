8b75236335419b88e14f34a173a3afdbefbe5b1d
Luis Lucero <lucero.luis@correounivalle.edu.co>
2025-06-29 14:05:14 -0500

    Modificación TurboAceleradaPar

--- src/main/scala/ReconstCadenasPar/package.scala
+++ src/main/scala/ReconstCadenasPar/package.scala
@@ -40,69 +40,95 @@ package object ReconstCadenasPar {
 
  */
 
-  def reconstruirCadenaTurboAcelerada(n: Int, o: Oraculo): Seq[Char] = {
-    // Recibe la longitud de la secuencia que hay que reconstruir (n, potencia de 2), y un oraculo para esa secuencia
-    // y devuelve la secuencia reconstruida
+  def reconstruirCadenaTurboAceleradaPar(n: Int, o: Oraculo, umbral: Int): Seq[Char] = {
     // Usa la propiedad de que si s = s1 ++ s2 entonces s1 y s2 también son subsecuencias de s
-    // Usa el filtro para ir más rápido
     // Usa árboles de sufijos para guardar Seq[Seq[Char]]
+    // Paraleliza según umbral
+
     def particiones(caracteres: Seq[Char], conjInicial: Seq[Seq[Char]], arbol: Trie): Boolean = {
       val tam = conjInicial.head.length
       val part = caracteres.sliding(tam).toList
-      if (part.forall(w => pertenece(w, arbol))) true
-      else false
+      part.forall(w => pertenece(w, arbol))
     }
 
     def filtrar(cadenas: Seq[Seq[Char]]): Seq[Seq[Char]] = {
       val arbol = arbolDeSufijos(cadenas)
-      for {
-        c1 <- cadenas
-        c2 <- cadenas
-        s = c1 ++ c2
-        if (particiones(s, cadenas, arbol))
-      } yield s
 
-    }
+      if (n <= umbral) {
+        for {
+          c1 <- cadenas
+          c2 <- cadenas
+          s = c1 ++ c2
+          if particiones(s, cadenas, arbol)
+        } yield s
+      } else {
+        val cadenasPar = cadenas.par
+        val (left, right) = cadenasPar.splitAt(cadenasPar.size / 2)
+        val ((l1, l2), (r1, r2)) = (left.splitAt(left.size / 2), right.splitAt(right.size / 2))
 
-    val filtrado = {
-      for {
-        cha <- alfabeto
-        if o(Seq(cha))
-        cor <- Seq(cha)
-      } yield cor
-    }
-
-    val combinaciones = for {
-      c1 <- filtrado
-      c2 <- filtrado
-    } yield Seq(c1, c2)
-
-
-    def recursivaTurboAcelerada(alfa: Seq[Seq[Char]]): Seq[Char] = {
-      val combinacionesSec = filtrar(alfa)
-      val filtrado = combinacionesSec.filter(x => o(x))
-      if (filtrado.head.length == n) filtrado.head
-      else {
-        recursivaTurboAcelerada(filtrado)
+        val (res1, res2, res3, res4) = parallel(
+          for {
+            c1 <- l1
+            c2 <- cadenas
+            s = c1 ++ c2
+            if particiones(s, cadenas, arbol)
+          } yield s,
+          for {
+            c1 <- l2
+            c2 <- cadenas
+            s = c1 ++ c2
+            if particiones(s, cadenas, arbol)
+          } yield s,
+          for {
+            c1 <- r1
+            c2 <- cadenas
+            s = c1 ++ c2
+            if particiones(s, cadenas, arbol)
+          } yield s,
+          for {
+            c1 <- r2
+            c2 <- cadenas
+            s = c1 ++ c2
+            if particiones(s, cadenas, arbol)
+          } yield s
+        )
+        (res1 ++ res2 ++ res3 ++ res4).seq
       }
     }
+
+    def recursivaTurboAceleradaPar(alfa: Seq[Seq[Char]]): Seq[Char] = {
+      val combinacionesSec = filtrar(alfa)
+
+      val filtrado = if (n <= umbral) {
+        combinacionesSec.filter(x => o(x))
+      } else {
+        val (left, right) = combinacionesSec.par.splitAt(combinacionesSec.size / 2)
+        val ((l1, l2), (r1, r2)) = (left.splitAt(left.size / 2), right.splitAt(right.size / 2))
+        val (fl1, fl2, fr1, fr2) = parallel(
+          l1.filter(o),
+          l2.filter(o),
+          r1.filter(o),
+          r2.filter(o)
+        )
+        (fl1 ++ fl2 ++ fr1 ++ fr2).seq
+      }
+
+      filtrado.find(_.length == n).getOrElse(recursivaTurboAceleradaPar(filtrado))
+    }
+
+    val conjuntoInicial = alfabeto.map(Seq(_))
 
     if (n == 1) {
-      filtrado
-    }
-    else if (n == 2) {
-      val filtrado2 = {
-        for {
-          cha <- combinaciones
-          if o(cha)
-          cor <- Seq(cha)
-        } yield cor
-      }
-      filtrado2.head
-    }
-
-    else {
-      recursivaTurboAcelerada(combinaciones)
+      conjuntoInicial.find(c => o(c)).getOrElse(Seq.empty)
+    } else if (n == 2) {
+      val combinaciones = for {
+        c1 <- conjuntoInicial
+        c2 <- conjuntoInicial
+        s = c1 ++ c2
+        if o(s)
+      } yield s
+      combinaciones.headOption.getOrElse(Seq.empty)
+    } else {
+      recursivaTurboAceleradaPar(conjuntoInicial)
     }
   }
-}
